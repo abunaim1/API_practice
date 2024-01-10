462f93c14f0a99ee60b8f953f13deb1848909404
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-10 23:40:44 +0600

    card adde

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-fetch("https://openapi.programming-hero.com/api/videos/category/1000")
-.then(res => res.json())
-.then(data => console.log(data))
+// const loadData = () => {
+//   fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data.data));
+// };
 
-const sortCard = sorting = () => {
-    console.log("HELLO");
-}
+const loadData = () => {
+  fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+    .then((res) => res.json())
+    .then((data) => displayData(data.data));
+};
+
+const displayData = (data) => {
+  const cardContainer = document.getElementById("card-container");
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.classList.add("card-individual");
+    card.innerHTML = `
+        <img class="thumbnail-image" src="${item.thumbnail}" alt="">
+        <div class="caption" >
+        <div><img class="caption-image" src="${item?.authors[0].profile_picture}" alt="profile"></div>
+        <div><p class="card-caption">${item.title}</p></div>
+        </div>
+        `;
+    cardContainer.appendChild(card);
+  });
+};
