cf5c4d18f2bbaf1883079e43b531d5215eb51fd9
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-11 20:42:50 +0600

    sort done

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
+// const loadData = () => {
+//   fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// };
+
 const loadData = (idChekar) => {
   document.getElementById("card-container").innerHTML = " ";
   document.getElementById("opps-container").innerHTML = " ";
   fetch(`https://openapi.programming-hero.com/api/videos/category/${idChekar}`)
     .then((res) => res.json())
-    .then((data) => displayData(data.data));
+    .then((data) => {
+      displayData(data.data);
+      sorting(data.data);
+    });
 };
 
 const displayData = (data) => {
-  let cardContainer = document.getElementById("card-container");
+  const cardContainer = document.getElementById("card-container");
   data.forEach((item) => {
     const card = document.createElement("div");
     card.classList.add("card-individual");
     card.innerHTML = `
         <img class="thumbnail-image" src="${item.thumbnail}" alt="">
+        <small class = "time">${item.others.posted_date ? formatTimeAgo(parseInt(item.others.posted_date)) : ""}</small>
         <div class="caption" >
         <div><img class="caption-image" src="${item?.authors[0].profile_picture}" alt="profile"></div>
         <div><p class="card-caption">${item.title}</p></div>
         </div>
         <div class = "author-name">
         <p>${item?.authors[0].profile_name} ${item.authors[0].varified ? '<i class="fa-solid fa-certificate"></i>' : " "}</p>
-        <p class = "author-name views">${item.others.views}</p>
+        <p class = "author-name views" id = "views-sort">${item.others.views}</p>
         </div>
         `;
     cardContainer.appendChild(card);
@@ -35,8 +45,20 @@ const opps = () => {
   `;
   oppsContainer.appendChild(oppsDetails);
 };
+
+function formatTimeAgo(minutes) {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  return `${hours} hours ${remainingMinutes} minutes ago`;
+}
+
 loadData("1000");
 
-const sortingBy = () =>{
-  
-}
+const sorting = (data) => {
+  const arr = [];
+  data.forEach((item) => {
+    arr.push(item);
+  });
+  const sortedArray = arr.sort((a, b) => parseInt(b.others.views) - parseInt(a.others.views));
+  displayData(sortedArray);
+};
